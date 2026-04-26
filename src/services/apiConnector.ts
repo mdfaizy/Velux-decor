1c6c78da4894b75d8508dfc468111c6b370aafc8
mdfaizy <mdgmfaizy@gmail.com>
2026-04-26 18:48:42 +0530

    change url

--- src/services/apiConnector.ts
+++ src/services/apiConnector.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // ✅ Base URL
 // const BASE_URL = "http://localhost:8080/api";
-const BASE_URL='http://velux-decor-server.onrender.com/api'
+const BASE_URL='https://velux-decor-server.onrender.com/api'
 
 // ✅ Axios instance
 const apiConnector = axios.create({
